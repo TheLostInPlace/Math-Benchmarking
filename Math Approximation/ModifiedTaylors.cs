5fd07166373d98ca0a79959da90274bdfbfe8ab1
TheLostInPlace <karam1.nasser@gmail.com>
2021-05-16 15:15:47 -0500

    Add project files.

---
+++ Math Approximation/ModifiedTaylors.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Math_Approximation
+{
+    class ModifiedTaylors
+    {
+        const double π = Math.PI;
+        const double π2 = Math.PI / 2;
+        const double π4 = Math.PI / 4;
+
+        public double Sin(double x)
+        {
+
+            if (x == 0) { return 0; }
+            if (x < 0) { return -Sin(-x); }
+            if (x > π) { return -Sin(x - π); }
+            if (x > π4) { return Cos(π2 - x); }
+
+            double x2 = x * x;
+
+            return x * (x2 / 6 * (x2 / 20 * (x2 / 42 * (x2 / 72 * (x2 / 110 * (x2 / 156 - 1) + 1) - 1) + 1) - 1) + 1);
+        }
+
+        public double Cos(double x)
+        {
+            if (x == 0) { return 1; }
+            if (x < 0) { return Cos(-x); }
+            if (x > π) { return -Cos(x - π); }
+            if (x > π4) { return Sin(π2 - x); }
+
+            double x2 = x * x;
+
+            return x2 / 2 * (x2 / 12 * (x2 / 30 * (x2 / 56 * (x2 / 90 * (x2 / 132 - 1) + 1) - 1) + 1) - 1) + 1;
+        }
+    }
+}
---
+++ Math Approximation/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Math_Approximation
+{
+    class Program
+    {
+
+        public static float convertRad(float angle)
+        {
+            return (float)(angle * Math.PI / 180);
+        }
+
+        private static long nanoTime()
+        {
+            long nano = 10000L * Stopwatch.GetTimestamp();
+            nano /= TimeSpan.TicksPerMillisecond;
+            nano *= 100L;
+            return nano;
+        }
+
+        public static Dictionary<string, long> startBenchmark(int maxLoops, int maxTime)
+        {
+
+            long startTime = DateTime.Now.Millisecond;
+
+            long taylorTime = 0;
+            long taylorModifiedTime = 0;
+            long csharpTime = 0;
+
+            TaylorMath taylorMath = new TaylorMath();
+
+            ModifiedTaylors modtaylorMath = new ModifiedTaylors();
+
+            Task calculateTaylor = Task.Factory.StartNew(() =>
+            {
+                taylorMath.sin(0);
+                taylorMath.cos(0);
+
+                //Console.WriteLine("Taylor Benchmark Started");
+
+                for (int i = 0; i < maxLoops; i++)
+                {
+                    float angle = (float)convertRad(i % 360);
+                    long time;
+
+                    time = nanoTime();
+
+                    taylorMath.sin(angle);
+                    taylorMath.cos(angle);
+
+                    taylorTime += nanoTime() - time;
+
+                    if (DateTime.Now.Millisecond - startTime > maxTime)
+                    {
+                        break;
+                    }
+
+                    //Console.WriteLine($"Loop value {i}");
+                }
+
+            }, new CancellationToken(false), TaskCreationOptions.LongRunning, TaskScheduler.Default);
+
+            Task calculateModifiedTaylor = Task.Factory.StartNew(() =>
+            {
+
+                modtaylorMath.Sin(0);
+                modtaylorMath.Cos(0);
+                //Console.WriteLine("Taylor Benchmark Started");
+
+                for (int i = 0; i < maxLoops; i++)
+                {
+                    float angle = (float)convertRad(i % 360);
+                    long time;
+
+                    time = nanoTime();
+
+                    modtaylorMath.Sin(angle);
+                    modtaylorMath.Cos(angle);
+
+                    taylorModifiedTime += nanoTime() - time;
+
+                    if (DateTime.Now.Millisecond - startTime > maxTime)
+                    {
+                        break;
+                    }
+
+                    //Console.WriteLine($"Loop value {i}");
+                }
+
+            }, new CancellationToken(false), TaskCreationOptions.LongRunning, TaskScheduler.Default);
+
+            Task calculateCSharp = Task.Factory.StartNew(() =>
+            {
+                Math.Sin(0);
+                Math.Cos(0);
+
+                //Console.WriteLine("CSharp Benchmark Started");
+
+                for (int i = 0; i < maxLoops; i++)
+                {
+                    float angle = (float)convertRad(i % 360);
+                    long time;
+
+                    time = nanoTime();
+
+                    Math.Sin(angle);
+                    Math.Cos(angle);
+
+                    csharpTime += nanoTime() - time;
+
+                    if (DateTime.Now.Millisecond - startTime > maxTime)
+                    {
+                        break;
+                    }
+
+                }
+
+            }, new CancellationToken(false), TaskCreationOptions.LongRunning, TaskScheduler.Default);
+
+
+            Task.WaitAll(new[] { calculateTaylor, calculateModifiedTaylor, calculateCSharp});
+
+            Dictionary<string, long> results = new Dictionary<string, long>();
+
+            results.Add("Taylors", taylorTime);
+            results.Add("Modified Taylors", taylorModifiedTime);
+            results.Add("CSharp", csharpTime);
+
+            return results;
+        }
+
+        public static string showResult(Dictionary<string, long> result)
+        {
+            StringBuilder results = new StringBuilder();
+
+            string worstTime = result.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+
+            long max = result.Max(x => x.Value);
+
+            long min = result.Min(x => x.Value);
+
+            foreach (KeyValuePair<string, long> dictionary in result)
+            {
+                results.Append($"Algorithm {dictionary.Key}, Time {dictionary.Value} ns \n ");
+            }
+
+            results.AppendLine($"Larger time is {worstTime}, with difference of {max-min} ns");
+
+            return results.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Starting benchmark: \n {showResult(startBenchmark(1_000_000, 10000))}");
+        }
+    }
+}
